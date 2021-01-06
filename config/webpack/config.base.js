7ead7e2d6e5191994f973316a29fe36fac53a239
Dain <dain.vandendool@hybrit.org>
2021-01-06 13:50:48 +0100

    adjust app title and, adjust misc global colors and, adjust app favicon and

--- config/webpack/config.base.js
+++ config/webpack/config.base.js
@@ -106,8 +106,8 @@ module.exports = ({ sourceDir, distDir }) => ({
     // PWA plugins
     new WebappWebpackPlugin({
       favicons: {
-        appDescription: "Storefront for the Saleor e-commerce platform",
-        appName: "Saleor",
+        appDescription: "Storefront for the HybrIT e-commerce platform",
+        appName: "HybrIT",
         background: "#ddd",
         developerURL: null, // prevent retrieving from the nearest package.json
         display: "standalone",
